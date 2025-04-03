37c78e5aca8cf7dffc8e1fce06e505b7e706bec9
Mansi Bhatnagar <bhatnagarmansi.03@gmail.com>
2025-04-03 11:05:32 +0530

    Stored content urls in files

--- DataFetch/reposNameFetch.py
+++ DataFetch/reposNameFetch.py
@@ -3,53 +3,52 @@ import json
 
 
 class repositoriesFetch:
-    def __init__(self,url,languages,headers):
+    def __init__(self, url, languages, headers):
         self.url = url
         self.languages = languages
         self.headers = headers
-        self.repositories = []
-        self.urls=[]
     
     def retrieveRepositories(self):
         # fetch the content based on both the languages
         for language in self.languages:
             try:
-                params = {
-                    'q': f'language:{language}',
-                    'sort': 'stars',
-                    'order': 'desc',
-                    'per_page': 1 
-                }
-    
-                reponse = requests.get(
-                    url= self.url,
-                    headers=self.headers,
-                    params=params
-                )
+                # for page in range(1, 6):
+                    params = {
+                        'q': f'language:{language}',
+                        'sort': 'stars',
+                        'order': 'desc',
+                        'per_page': 100,
+                        'page': 5
+                    }
+        
+                    response = requests.get(
+                        url= self.url,
+                        headers=self.headers,
+                        params=params
+                    )
 
-                reponse.raise_for_status()
-                self.repositories.append(reponse.json())
-
-                for repo in self.repositories:
-                    if(repo['url']):
-                        self.urls.append(repo['url'])
-                        print(repo['url'])
+                    response.raise_for_status()
+                    self.cleanData(response.json(), language)
 
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching data from GitHub API: {e}")
                 return []
-    
-    def printData(self):
-        print("repos, urls = ", self.repositories, self.urls)
 
+    def cleanData(self, repo, language):
+        urls = [item['contents_url'][:-7] for item in repo['items']]
+        filename = f"{language.lower()}Data.json"
 
-    def cleanData(self):
-        """
-        1. understand the API reponse structure
-        2. extract the urls of the repositories
-        3. store the urls of the repositories in the json files
-        """
+        try:
+            with open(filename, 'r') as f:
+                existing_urls = json.load(f)
+        except FileNotFoundError:
+            existing_urls = []
 
+        combined_urls = list(set(existing_urls + urls))
+
+        with open(filename, 'w') as f:
+            json.dump(combined_urls, f, indent=2)
+            
 
 headers = {
         'Accept': 'application/vnd.github.v3+json',
@@ -64,4 +63,3 @@ repositoriesFetcher = repositoriesFetch(
 )
 
 repositoriesFetcher.retrieveRepositories()
-# repositoriesFetcher.printData()
